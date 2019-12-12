7b4c5d7ceae29b135ba25182ce82149d6742cf51
brokenmold <jason@broken-mold.com>
2019-12-12 16:31:43 +0000

    Hello World!

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import React from "react";
 import "./Header.scss"
 
-
+const Header = () => (
+    <div className="Header">(Header Here)</div>
+);
 
 export default Header;
