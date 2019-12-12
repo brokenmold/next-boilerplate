3b6c176a522ab358dbcd326720b3f06045ecb789
brokenmold <jason@broken-mold.com>
2019-12-12 16:02:48 +0000

    Added Components Structure

---
+++ components/Header.js
@@ -0,0 +1,3 @@
+import Link from 'next/link';
+import React from "react";
+import "./Header.scss"
---
+++ components/_layout.js
@@ -0,0 +1,3 @@
+import Head from "next/head";
+import "./Layout.scss";
+import Header from "./Header";
