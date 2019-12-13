179c56e8125f37835a282d9103a1cac02d414398
brokenmold <jason@broken-mold.com>
2019-12-13 21:59:12 +0000

    Added Meta Data

--- components/_Layout.js
+++ components/_Layout.js
@@ -3,13 +3,20 @@ import Head from "next/head";
 import Link from 'next/link';
 
 import "./_Layout.scss";
+
 import Header from "./Header";
 
 const Layout = () => (
     <div className="Layout">
-        <p>(Layout Here)</p>
+
+        <Head>
+            <title>Next-Boilerplate</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta charSet="utf-8" />
+        </Head>
 
         <Header />
+        
     </div>
 );
 
