72dd158b30126e0babee475233fd5cfab3d83b0e
brokenmold <jason@broken-mold.com>
2019-12-13 21:55:37 +0000

    Manage Imports

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,9 @@
-import Link from 'next/link';
-import React from "react";
 import "./Header.scss"
 
 const Header = () => (
-    <div className="Header">(Header Here)</div>
+    <div className="Header">
+        (Header Here)
+    </div>
 );
 
 export default Header;
--- components/_Layout.js
+++ components/_Layout.js
@@ -1,11 +1,14 @@
+import React from "react";
 import Head from "next/head";
+import Link from 'next/link';
+
 import "./_Layout.scss";
 import Header from "./Header";
 
 const Layout = () => (
     <div className="Layout">
         <p>(Layout Here)</p>
-        
+
         <Header />
     </div>
 );
